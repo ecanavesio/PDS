6fb4a3696079f6171a2257ab2eeeac2245ec9b44
emcanavesio <canavesio.em@protonmail.com>
2021-02-08 08:57:11 -0300

    Multiplot + Layout 2x2 into TP1-EJ3

--- tp1/ej3.js
+++ tp1/ej3.js
@@ -9,28 +9,38 @@ const s2 = sampledSinWithT(1, 5, 0, 100, 0, 1);
 const s3 = sampledSinWithT(1, 5, 0, 25, 0, 1);
 const s4 = sampledSinWithT(1, 5, 0, 4, 0, 1);
 
-gnuplot.plot([
-  {
-    data: s1.t.pair(s1.x),
-    style: "lines",
-    title: "1000 Hz"
-  },
-  {
-    data: s2.t.pair(s2.x),
-    style: "lines",
-    title: "100 Hz"
-  },
-  {
-    data: s3.t.pair(s3.x),
-    style: "lines",
-    title: "25 Hz"
-  },
-  {
-    data: s4.t.pair(s4.x),
-    style: "lines",
-    title: "4 Hz"
-  }
-]);
+gnuplot
+  .print("set multiplot layout 2,2\n")
+  .print("set tmargin 2 \n")
+  .print(`set title "1000 Hz"\n`)
+  .print("unset key\n")
+  .plot([
+    {
+      data: s1.t.pair(s1.x),
+      style: "lines"
+    }
+  ])
+  .print(`set title "100 Hz"\n`)
+  .plot([
+    {
+      data: s2.t.pair(s2.x),
+      style: "lines"
+    }
+  ])
+  .print(`set title "25 Hz"\n`)
+  .plot([
+    {
+      data: s3.t.pair(s3.x),
+      style: "lines"
+    }
+  ])
+  .print(`set title "4 Hz"\n`)
+  .plot([
+    {
+      data: s4.t.pair(s4.x),
+      style: "lines"
+    }
+  ]);
 
 gnuplot.set({
   xrange:"[0:1]",
